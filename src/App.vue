fb62bf4676aca73330ad28243e828e1d028010a6
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-29 17:04:06 +0200

    search Tv Series implemented

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
    />
    <main-boolflix 
     :arr-movie-prop="movieArr"
+    :arr-serie-prop="serieArr"
    />
     
   </div>
@@ -25,15 +26,17 @@ export default {
     return {
       querySearch: '',
       movieApi: 'https://api.themoviedb.org/3/movie/now_playing?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&page=1',
+      serieApi: 'https://api.themoviedb.org/3/movie/latest?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&page=1',
       responseDelay: 10,
       movieArr: [],
+      serieArr: [],
     }
   },
   created () {
     
   },
   methods: {
-    // Fa Requesta Api
+    // Fa Requesta Api Per Movie
     requestApiMovie () {
       this.movieArr = [],
       setTimeout (() => {
@@ -45,11 +48,25 @@ export default {
           })
       },this.responseDelay)
     },
+    // Fa Requesta Api Per Serie
+    requestApiSerie () {
+      this.serieArr = [],
+      setTimeout (() => {
+        axios.get(this.serieApi)
+          .then((responseMovie) => {
+            responseMovie.data.results.forEach(serie => {
+              this.serieArr.push(serie);
+            });
+          })
+      },this.responseDelay)
+    },
     querySearchStringPass(querySearchString) {
       this.querySearch = querySearchString;
       this.movieApi = `https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&query=${querySearchString}&page=1&include_adult=false`,
+      this.serieApi = `https://api.themoviedb.org/3/search/tv?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&page=1&query=${querySearchString}&page=1&include_adult=false`,
       console.log(querySearchString)
       this.requestApiMovie ();
+      this.requestApiSerie ();
     },
   },
   props: {
--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="container-fluid">
-    <div class="row row-cols-3 g-4">
-      <cards-boolflix 
-        v-for="movie in this.arrMovieProp"
-        :key="movie.id"
-        :moviePoster="movie.poster_path"
-        :movieId="movie.id"
-        :movieTitle="movie.title" 
-        :movieTitleOriginal="movie.original_title" 
-        :movieLang="movie.original_language" 
-        :movieRating="movie.vote_average" 
-      />
+    <div class="container">
+      <h2>Movie</h2>
+      <div class="row row-cols-3 g-4">
+        <cards-boolflix 
+          v-for="movie in this.arrMovieProp"
+          :key="movie.id"
+          :moviePoster="movie.poster_path"
+          :movieId="movie.id"
+          :movieTitle="movie.title" 
+          :movieTitleOriginal="movie.original_title" 
+          :movieLang="movie.original_language" 
+          :movieRating="movie.vote_average" 
+        />
 
+      </div>
+    </div>
+    <div class="container">
+      <h2>Serie TV</h2>
+      <div class="row row-cols-3 g-4">
+        <cards-boolflix 
+          v-for="movie in this.arrSerieProp"
+          :key="movie.id"
+          :moviePoster="movie.poster_path"
+          :movieId="movie.id"
+          :movieTitle="movie.title" 
+          :movieTitleOriginal="movie.original_title" 
+          :movieLang="movie.original_language" 
+          :movieRating="movie.vote_average" 
+        />
+
+      </div>
     </div>
   </div>
 </template>
@@ -31,6 +50,7 @@ export default {
   },
   props: {
     arrMovieProp: Array,
+    arrSerieProp: Array,
   }
 
 }
