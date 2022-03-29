d1fdb87f01f521f51423a59ce1eed7f705a0d332
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-29 16:45:42 +0200

    language flag checker implemented

--- src/components/CardsBoolflix.vue
+++ src/components/CardsBoolflix.vue
@@ -4,7 +4,17 @@
       <img class="img-fluid" :src="`https://image.tmdb.org/t/p/original/${moviePoster}`" :alt="movieTitle">
       <h3> {{movieTitle}} </h3>
       <h4> {{movieTitleOriginal}}</h4>
-      <lang-flag :iso="movieLang" squared="false"/>
+      <div class="language-holder">
+        <lang-flag v-if="movieLanguageCheck(movieLang)" :iso="movieLang"/>
+        <div v-else>
+          <div class="d-flex align-items-center-2">
+            <img class="me-1" height="20px" src="https://icons-for-free.com/iconfiles/png/512/globe+international+work+world+icon-1320086521784287131.png" alt="">
+            <span>language flag not detected </span>
+          </div>
+          
+          <p>language: {{movieLang}}</p>
+        </div>
+      </div>
       <div class="voto">{{movieRating}}</div>
    </div>
  </div>
@@ -13,13 +23,27 @@
 <script>
 export default {
   name: 'CardsBoolflix',
+  data () {
+    return {
+      langCheckArr: [
+        'ar','am','az','be','bn','bg','zh','ca','cs','en','et','fr','de','ha','hi','hu','it',
+    'ja','jv','km','ko','lv','ms','mr','fa','pl','pt','ro','ru','es','sw','ta','te','th','tr','uz','vi',
+      ]
+    }
+  },
   props: {
     moviePoster: String,
     movieId: Number,
     movieTitle: String,
     movieTitleOriginal: String,
     movieLang: String,
-    movieRating: String,
+    movieRating: Number,
+  },
+  methods: {
+    //Movie Language Check
+    movieLanguageCheck(lang) {
+      return this.langCheckArr.includes(lang)
+    }
   }
 }
 </script>
